416e6a1cd3632bae893fd01d33193862c244d56f
wangable404 <wangable404@gmail.com>
2026-01-31 18:14:26 +0500

    f

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-require('dotenv').config()
-const express = require('express')
-const sequelize = require('./db')
-const cors = require('cors')
-const model = require('./models/model')
-const fileUpload = require('express-fileupload');
-const errorHeandler = require('./middleware/ErrorHeadlingMiddleware')
-const router = require('./routes/index')
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./db");
+const cors = require("cors");
+const model = require("./models/model");
+const fileUpload = require("express-fileupload");
+const errorHeandler = require("./middleware/ErrorHeadlingMiddleware");
+const router = require("./routes/index");
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.json());
+app.use(fileUpload());
+app.use("/api/v1/", router);
 
 app.use(errorHeandler);
 
+app.get("/", (req, res) => {
+  res.send("Dashboard is running!");
+});
+
 const start = async () => {
-    try {
-        await sequelize.authenticate()
-        await sequelize.sync()
-        app.get('/', (req, res) => {
-            res.send('Dashboard is running!')
-        })
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
 
-        app.use(express.json())
-        app.use(fileUpload());
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log("Error connecting to the database:", err);
+  }
+};
 
-        app.use('/api/v1/', router)
-
-        app.listen(PORT, '0.0.0.0', () => {
-            console.log(`Server is running on port ${PORT}`)
-        })
-    } catch (err) {
-        console.log('Error connecting to the database:', err);
-    }
-}
-
-start()
+start();
