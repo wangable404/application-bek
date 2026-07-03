b13c2d69773cfbeb566465c64da408dad403af80
wangable404 <wangable404@gmail.com>
2026-07-03 19:43:12 +0500

    fullname

--- services/notify.service.js
+++ services/notify.service.js
@@ -1,6 +1,6 @@
-import { PushToken, TelegramChat } from "../models/model";
-import { sendPush } from "./push.service";
-import { tgSendMessage } from "./telegram.service";
+import { PushToken, TelegramChat } from "../models/model.js";
+import { sendPush } from "./push.service.js";
+import { tgSendMessage } from "./telegram.service.js";
 
 export async function notifyUser(userId, title, body, data = {}) {
   const [pushTokens, tgChats] = await Promise.all([
